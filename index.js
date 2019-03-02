53881296f137789db6b86ec8092e03ea240c9aa1
Fedor Paretsky <me@f3d0r.com>
2019-03-02 09:40:20 -0800

    Update index.js to promises

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const {
+    google
+} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
@@ -11,9 +13,9 @@ const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), listLabels);
+    if (err) return console.log('Error loading client secret file:', err);
+    // Authorize a client with credentials, then call the Gmail API.
+    authorize(JSON.parse(content), listLabels);
 });
 
 /**
@@ -23,16 +25,20 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    const {
+        client_secret,
+        client_id,
+        redirect_uris
+    } = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
 }
 
 /**
@@ -42,28 +48,28 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
     });
-  });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) return console.error('Error retrieving access token', err);
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log('Token stored to', TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
 }
 
 //subject for email titles, body for the content
@@ -76,46 +82,69 @@ var body = [];
  */
 async function listLabels(auth) {
 
-  const gmail = google.gmail({version: 'v1', auth});
+    const gmail = google.gmail({
+        version: 'v1',
+        auth
+    });
 
-  //list messages and id numbers, and use those id numbers to get each message
-  gmail.users.messages.list({
-    'userId': 'me'
-  }, (err, res) => {
-    if(err) return console.log("Error: " + err);
-    const messages = res.data.messages;
-    //console.log(messages);
-    // now parse through each message
-    for(let i = 0; i < messages.length; i++) {
-      //getEmails(gmail, messages[i]['id']);
-      gmail.users.messages.get({
-        'userId': 'me',
-        'id': messages[i]['id']
-      }, async (err, res) => {
-        if(err) return console.log(err);
-        //else no error so decode the base64 message inside the email
-        //but make sure its not an empty email
-        if(res.data.payload.parts[0].body.data != null) {
-          await body.push(new Buffer.from(res.data.payload.parts[0].body.data, 'base64').toString() + "\n");
-        } else { //email has multimedia so its pushed to somewhere else
-          await body.push(new Buffer.from(res.data.payload.parts[0].parts[0].body.data, 'base64').toString());
-        }
+    //list messages and id numbers, and use those id numbers to get each message
+    var res = await new Promise(function (resolve, reject) {
+        gmail.users.messages.list({
+            'userId': 'me'
+        }, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
 
-        //runs into a problem where result will execute too fast or some messages
-        //are skippede over from gmail
-        result(messages.length, i);
-      }); //ends particular gmail.messages.get
+    // FORMING REQUESTS
+    var reqs = [];
+    res.data.messages.forEach(function (message) {
+        reqs.push(new Promise(function (resolve, reject) {
+            gmail.users.messages.get({
+                'userId': 'me',
+                'id': message.id
+            }, function (err, res) {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (res.data.payload.parts[0].body.data != null) {
+                        resolve(new Buffer.from(res.data.payload.parts[0].body.data, 'base64').toString() + "\n");
+                    } else { //email has multimedia so its pushed to somewhere else
+                        resolve(new Buffer.from(res.data.payload.parts[0].parts[0].body.data, 'base64').toString());
+                    }
+                }
+            });
+        }));
+    });
+    console.log(`LENGTH: ${res.data.messages.length}`);
 
-    } //ends for loop
+    // GETTING MESSAGES
+    var results = await Promise.all(reqs);
+    console.log(`LENGTH OF RETURNED EMAILS: ${results.length}`);
 
-  }); //ends messages.list
-
+    // FILE OUTPUT
+    var textOutput = "";
+    results.forEach(function (currentEmail) {
+        textOutput += currentEmail;
+    });
+    await new Promise((resolve, reject) => {
+        fs.writeFile('output-email.txt', textOutput, function (err) {
+            if (err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
 }
 
-//prints email. needed to do this as a callback function
-//print only if a == b
-function result(a, b) {
-  if(a - 1 == b) {
-    fs.writeFileSync("emailOUTPUT", body);
-  }
-}
+
+
+// Things we need in JSON file after filtering emails
+// message
+// sender
+// domain of sender (the end of the email) - the company
+// status (later)
